064a73365363ce7f84305216a832048b84873495
kapantzak <i.kapantzakis@epsilonnet.gr>
2020-01-14 16:38:44 +0200

    Adds more test cases for classExtendsReact method

--- __tests__/classExtendsReact.test.js
+++ __tests__/classExtendsReact.test.js
@@ -1,7 +1,27 @@
+const { types: t } = require("@babel/core");
 const { classExtendsReact } = require("../src/helpers/classExtendsReact");
 
 describe("classExtendsReact()", () => {
-  it("Should pass", () => {
-    expect(1).toBe(1);
+  it("Should return false if superclass is null", () => {
+    const classDec = t.classDeclaration("Test", null, null);
+    const actual = classExtendsReact(classDec);
+    expect(actual).toBe(false);
+  });
+
+  test.each([
+    ["Component", t.identifier("Component")],
+    ["PureComponent", t.identifier("PureComponent")],
+    [
+      "React.Component",
+      t.memberExpression(t.identifier("React"), t.identifier("Component"))
+    ],
+    [
+      "React.PureComponent",
+      t.memberExpression(t.identifier("React"), t.identifier("PureComponent"))
+    ]
+  ])("Should return true if superclass is '%s'", (_, superClassNode) => {
+    const classDec = t.classDeclaration("Test", superClassNode, null);
+    const actual = classExtendsReact(classDec);
+    expect(actual).toBe(true);
   });
 });
